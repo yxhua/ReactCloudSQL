1b47cb16f699950d67de528351f2d18aad7a8d45
Michael H <michael.hua@zoominfo.com>
2021-07-22 09:17:27 -0400

    Updated to r2dbc connector 1.3.1

--- src/main/java/com/dozi/zoominfo/service/DBHeartBeat.java
+++ src/main/java/com/dozi/zoominfo/service/DBHeartBeat.java
@@ -3,28 +3,26 @@ package com.dozi.zoominfo.service;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.r2dbc.core.DatabaseClient;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import io.r2dbc.spi.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @Slf4j
-@Configuration
-@EnableScheduling
+@Component
 public class DBHeartBeat {
 	@Autowired
 	ConnectionFactory connectionFactory;
 
-	@Scheduled(initialDelay=60000, fixedRate = 60000)
+	//@Scheduled(initialDelay = 5000, fixedRate = 5000)
 	public void heartbeat() {
 		DatabaseClient client = DatabaseClient.create(connectionFactory);
-		
+
 		Mono<Map<String, Object>> result = client.sql("select now() as now").fetch().first();
-		
+
 		log.info("Getting DB time: {}", result.block().get("now"));
 	}
 
